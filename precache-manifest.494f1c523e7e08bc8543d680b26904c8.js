e331c6ba52c802d1cf980152fff34169c5c16594
zeffgo <j0nybr4v0@gmail.com>
2019-04-15 14:44:57 +0300

    test

---
+++ precache-manifest.494f1c523e7e08bc8543d680b26904c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "521039ba6d02e70dcb96",
+    "url": "/static/css/main.91f7ef69.chunk.css"
+  },
+  {
+    "revision": "521039ba6d02e70dcb96",
+    "url": "/static/js/main.cc3f7c95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "984485da9374c07a7ca0",
+    "url": "/static/js/2.531da718.chunk.js"
+  },
+  {
+    "revision": "b5e0316a69b1915270d313202aece2bb",
+    "url": "/index.html"
+  }
+];
